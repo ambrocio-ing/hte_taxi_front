51dfbbc1c3d8518c31ced960cc1cb399979b4c7c
kaliuser <kaliuser@userOne.UserOne>
2022-04-20 09:23:56 -0500

    se cambio url de config

--- src/app/componentes/sistema/config/config.ts
+++ src/app/componentes/sistema/config/config.ts
@@ -1,2 +1,2 @@
-export const URL_BACKEND = "https://transporteseguro.ml:8445";
+export const URL_BACKEND = "https://www.transporteseguro.ml:8445";
 //export const URL_BACKEND = "http://localhost:8080";
